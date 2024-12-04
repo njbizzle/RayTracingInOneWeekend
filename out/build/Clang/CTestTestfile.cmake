8516bb44313fb9206bebfcf5843d0f7584079f47
nmurphy <nmurphy.mine@gmail.com>
2024-12-03 19:29:16 -0800

    reordering

--- out/build/Clang/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/natemurphy/Desktop/projects/cpp/RayTracingInOneWeekend
-# Build directory: /Users/natemurphy/Desktop/projects/cpp/RayTracingInOneWeekend/out/build/Clang
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
